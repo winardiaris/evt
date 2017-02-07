3be751634a8d9b563a222030972ca048e92764b8
Aris Winardi <ariswinardy@gmail.com>
2017-02-07 14:58:24 +0700

    add model for facebook login

---
+++ app/SocialAccountService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Laravel\Socialite\Contracts\User as ProviderUser;
+use Illuminate\Support\Facades\Hash;
+
+class SocialAccountService
+{
+  public function createOrGetUser(ProviderUser $providerUser)
+  {
+    $account = SocialAccount::whereProvider('facebook')
+      ->whereProviderUserId($providerUser->getId())
+      ->first();
+
+    if ($account) {
+      return $account->user;
+    } else {
+      $account = new SocialAccount([
+        'provider_user_id' => $providerUser->getId(),
+        'provider' => 'facebook'
+
+      ]);
+
+      $user = User::whereEmail($providerUser->getEmail())->first();
+      if (!$user) {
+
+        $user = User::create([
+          'email' => $providerUser->getEmail(),
+          'name' => $providerUser->getName(),
+          'password' =>  Hash::make($providerUser->getId()),
+
+        ]);
+      }
+
+      $account->user()->associate($user);
+      $account->save();
+
+      return $user;
+    }
+  }
+}
