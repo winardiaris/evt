6cefa00bbe0204419938d62f9a35c48812fcc561
Aris Winardi <ariswinardy@gmail.com>
2017-03-01 17:07:16 +0700

    change redirect to /

---
+++ app/Http/Controllers/GeneralController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GeneralController extends Controller
+{
+  //
+  public static function uploadImage($file){
+    $urlImage = "urlImage";
+    return $urlImage;
+  }
+}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\User;
+use App\UsersProfile;
+
 
 class HomeController extends Controller
 {
