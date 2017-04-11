574ea9823aabc1115e5c60cb9a70d9af9f4b6e4a
Aris Winardi <ariswinardy@gmail.com>
2017-04-11 19:43:05 +0700

    move to generalcontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 /* use Illuminate\Support\Facades\Auth; */
-use Auth;
-use App\User;
-use App\UsersProfile;
-
 
 class HomeController extends Controller
 {
@@ -28,24 +24,7 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-      if (Auth::check()) {
-        $user = User::find(Auth::id())->first();
-        $profiles = (new ProfileController)->getUserProfile(Auth::user()->id);
-        $options = (new OptionsController)->getUserOption(Auth::user()->id);
-
-        session([
-          'user_id'=>Auth::id(),
-          'username'=>Auth::user()->username,
-          'name'=>Auth::user()->name,
-        ]);
-        if(isset($profiles)){
-          if(count($profiles)>0){
-            foreach($profiles as $key => $value){
-              session([$key=>$value]);
-            }
-          }
-        }
-      }
-        return view('welcome');
+      (new GeneralController)->userAutoLoad();
+      return view('welcome');
     }
 }
