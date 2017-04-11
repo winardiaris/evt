cea2ad10424b118a9afe4bb065ff116546317f79
Aris Winardi <ariswinardy@gmail.com>
2017-04-11 19:43:22 +0700

    add userAutoLoad

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\User;
+use App\UsersProfile;
 
 class GeneralController extends Controller
 {
@@ -10,5 +13,26 @@ class GeneralController extends Controller
   public static function uploadImage($file){
     $urlImage = "urlImage";
     return $urlImage;
+  }
+  public function userAutoLoad(){
+      if (Auth::check()) {
+        $user = User::find(Auth::id())->first();
+        $profiles = (new ProfileController)->getUserProfile(Auth::user()->id);
+        $options = (new OptionsController)->getUserOption(Auth::user()->id);
+
+        session([
+          'user_id'=>Auth::id(),
+          'username'=>Auth::user()->username,
+          'name'=>Auth::user()->name,
+        ]);
+        if(isset($profiles)){
+          if(count($profiles)>0){
+            foreach($profiles as $key => $value){
+              session([$key=>$value]);
+            }
+          }
+        }
+      }
+
   }
 }
