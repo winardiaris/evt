2fd232225cc3280a2aa1a34b22c9704dc631d39b
Aris Winardi <ariswinardy@gmail.com>
2017-04-20 17:28:43 +0700

    change avatar animation

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
-  $("div.isme img").click(function(){
-  console.log("avatar click");
+  $("div.isme .click-me").click(function(){
     $(".inputAvatar").click();
   });
+
   $("div.isme .inputAvatar").change(function(){
     $('.avatarSubmit').click();
   });
+
   $('input#username').keyup(function(){
     console.log($(this).val());
     $('input[name=usr]').val($(this).val());
