fa98ba4a07ac55b40144a8fd06db82022b8cf4ea
Aris Winardi <ariswinardy@gmail.com>
2017-04-28 17:29:02 +0700

    simplify title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }} @yield('title')</title>
+    <title>(Beta) @yield('title')</title>
 
     <!-- Styles -->
     <link href="{{asset('css/all.css')}}" rel="stylesheet">
