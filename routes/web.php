8e779f0d67867703517644bce6f185333252458a
Aris Winardi <ariswinardy@gmail.com>
2017-04-28 17:28:42 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -11,29 +11,36 @@
 |
 */
 
-Route::get('/', ['as'=>'home','uses'=>'HomeController@index']);
+Route::get('/', ['as' => 'home','uses' => 'HomeController@index']);
 
 Auth::routes();
 // General
-Route::get('/{username}/',['as'=>'profile-view','uses'=> 'ProfileController@view']);
-Route::get('/search/{search}/',['as'=>'search-get','uses'=> 'GeneralController@searchGet']);
-Route::post('/search/',['as'=>'search-post','uses'=> 'GeneralController@searchPost']);
+Route::get('/{username}/',['as' => 'profile-view','uses' => 'ProfileController@view']);
+Route::get('/search/{search}/',['as' => 'search-get','uses' => 'GeneralController@searchGet']);
+Route::post('/search/',['as' => 'search-post','uses' => 'GeneralController@searchPost']);
+Route::get('/{username}/post/{id}',['as' => 'post-view','uses' => 'PostController@view']);
+Route::get('/search/category/{id}',['as' => 'category-view','uses' => 'PostController@category']);
 
 // Route::get('/home', 'ProfileController@getAvatar');
 
 // Social Login =================
 $s = 'social.';
-Route::get('/a/{provider}', ['as'=>$s.'redirect','uses'=>'SocialAuthController@redirect']);
-Route::get('/cb/{provider}', ['as'=>$s.'callback','uses'=>'SocialAuthController@callback']);
+Route::get('/a/{provider}', ['as'=>$s.'redirect','uses' => 'SocialAuthController@redirect']);
+Route::get('/cb/{provider}', ['as'=>$s.'callback','uses' => 'SocialAuthController@callback']);
 
 
 
 
 // Middleware user logged in
-Route::group(['middleware'=>'CheckUser'],function(){
-  Route::get('/{username}/profile/edit',['as'=>'profile-edit','uses'=> 'ProfileController@edit']);
-  Route::post('/{username}/u/av',['as'=>'avatar-update','uses'=> 'ProfileController@avatarUpdate']);
-  Route::post('/{username}/profile/update',['as'=>'profile-update','uses'=> 'ProfileController@update']);
+Route::group(['middleware' => 'CheckUser'],function(){
+  Route::get('/{username}/profile/edit',['as' => 'profile-edit','uses' => 'ProfileController@edit']);
+  Route::post('/{username}/u/av',['as' => 'avatar-update','uses' => 'ProfileController@avatarUpdate']);
+  Route::post('/{username}/profile/update',['as' => 'profile-update','uses' => 'ProfileController@update']);
+
+  //posts
+  Route::post('/{username}/post/new/',['as' => 'post-new','uses' => 'PostController@save']);
+  Route::get('/{username}/post/{id}/edit',['as' => 'post-edit','uses' => 'PostController@edit']);
+  Route::post('/{username}/post/update/',['as' => 'post-update','uses' => 'PostController@update']);
 });
 
 
@@ -43,7 +50,7 @@ Route::group(['middleware' =>'isAdmin'], function () {
 
 });
 
-Route::post('/a/f/{username}',['as'=>'add-friend','uses'=>'FriendListController@addfriend']);
+Route::post('/a/f/{username}',['as' => 'add-friend','uses' => 'FriendListController@addfriend']);
 
 // Pages =================
-Route::get('/pages/donate', ['as'=>'donate','uses'=>'GeneralController@donate']);
+Route::get('/pages/donate', ['as' => 'donate','uses' => 'GeneralController@donate']);
