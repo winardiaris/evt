e036d834c9166465df4f90499c5b9429fba0e75c
Aris Winardi <ariswinardy@gmail.com>
2017-04-28 18:41:41 +0700

    add load more

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,24 @@
 @section('content')
 <div class="container">
   <div class="row">
-    <div class="waterfall" data-col-min-width="300"
-      data-default-container-width="400"
-      data-autoresize="true">
-      @foreach ($posts as $post)
-        <div class="col-md-4 item">
-          @include('post.post')
-        </div>
-      @endforeach
+    <div class="col-md-12">
+      <div class="waterfall" data-col-min-width="300"
+        data-default-container-width="400"
+        data-autoresize="true">
+        @foreach ($posts as $post)
+          <div class="col-md-4 item">
+            @include('post.post')
+          </div>
+        @endforeach
+      </div>
     </div>
+    @if(Auth::guest())
+    <div class="col-md-12">
+      <div class="col-md-4 col-md-offset-4">
+        <a href="{{url('/login')}}"class="btn btn-default btn-block">Load More..</a>
+      </div>
+    </div>
+    @endif
   </div>
 </div>
 @endsection
