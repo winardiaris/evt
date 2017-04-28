e04858a598ee15ed2fb52da06db9101743697640
Aris Winardi <ariswinardy@gmail.com>
2017-04-28 17:28:16 +0700

    show pos on home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,14 @@
 @section('content')
 <div class="container">
   <div class="row">
-    <div class="col-md-4 col-md-offset-4">
-      <img class="welcome-logo" src="{{asset('img/logo.png')}}">
-<br>"Disini nanti buat list event terdekat"
+    <div class="waterfall" data-col-min-width="300"
+      data-default-container-width="400"
+      data-autoresize="true">
+      @foreach ($posts as $post)
+        <div class="col-md-4 item">
+          @include('post.post')
+        </div>
+      @endforeach
     </div>
   </div>
 </div>
