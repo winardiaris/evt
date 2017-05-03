c2f981d2cc16b42f7a302a4f1feb910ef356b91f
Aris Winardi <ariswinardy@gmail.com>
2017-05-03 16:09:08 +0700

    update view

--- resources/views/post/form.blade.php
+++ resources/views/post/form.blade.php
@@ -1,27 +1,14 @@
 <div class="col-md-12">
   <div class="form-group {!! $errors->has('title') ? 'has-error':'' !!}">
-    {!!Form::label('title','Event Name') !!}
+    {!!Form::label('title','Event Title') !!}
     {!!Form::text('id',null,['hidden'=>'true']) !!}
     {!!Form::text('title',null,['class'=>'form-control','placeholder'=>'Event Name']) !!}
     {!! $errors->first('title','<p class="help-block">:message</p>')!!}
   </div>
 </div>
 <div class="col-md-12">
-  <div class="form-group {!! $errors->has('description') ? 'has-error':'' !!}">
-    {!!Form::label('description','Description') !!}
-    {!!Form::text('description',null,['class'=>'form-control']) !!}
-    {!! $errors->first('description','<p class="help-block">:message</p>')!!}
-  </div>
-</div>
-<div class="col-md-12">
-  <div class="form-group">
-  <label for="file-1">Image</label>
-    <input id="file-1" name="photo[]" type="file" multiple class="file" data-overwrite-initial="false" data-min-file-count="2">
-  </div>
-</div>
-<div class="col-md-12">
   <div class="form-group {!! $errors->has('place') ? 'has-error':'' !!}">
-    {!!Form::label('place','Place') !!}
+    {!!Form::label('place','Location') !!}
     {!!Form::text('place',null,['class'=>'form-control']) !!}
     {!! $errors->first('place','<p class="help-block">:message</p>')!!}
   </div>
@@ -29,6 +16,9 @@
 <div class="col-md-4">
   <div class="form-group {!! $errors->has('currency_id') ? 'has-error':'' !!}">
     {!!Form::label('currency_id','Currency') !!}
+    {!!Form::select('currency_id',$currency,
+      !isset($post['currency_id']) ? '':$post['currency_id'],
+      ['class'=>'form-control']) !!}
     {!! $errors->first('currency_id','<p class="help-block">:message</p>')!!}
   </div>
 </div>
@@ -41,7 +31,7 @@
 </div>
 <div class="col-md-6">
   <div class="form-group {!! $errors->has('time_start') ? 'has-error':'' !!}">
-    {!!Form::label('time_start','Start') !!}
+    {!!Form::label('time_start','Starts') !!}
     <div class="input-group date" id="timestart">
       {!!Form::text('time_start',null,['class'=>'form-control']) !!}
       <span class="input-group-addon">
@@ -53,7 +43,7 @@
 </div>
 <div class="col-md-6">
   <div class="form-group {!! $errors->has('time_end') ? 'has-error':'' !!}">
-    {!!Form::label('time_end','End') !!}
+    {!!Form::label('time_end','Ends') !!}
     <div class="input-group date" id="timeend">
       {!!Form::text('time_end',null,['class'=>'form-control']) !!}
       <span class="input-group-addon">
@@ -62,6 +52,20 @@
     </div>
     {!! $errors->first('time_end','<p class="help-block">:message</p>')!!}
   </div>
+</div>
+<div class="col-md-12">
+  <div class="form-group">
+  <label for="file-1">Image</label>
+    <input id="file-1" name="photo[]" type="file" multiple class="file" data-overwrite-initial="false" data-min-file-count="2">
+  </div>
+</div>
+<div class="col-md-12">
+  <div class="form-group {!! $errors->has('description') ? 'has-error':'' !!}">
+    {!!Form::label('description','Event Description') !!}
+    {!!Form::textarea('description',null,['class'=>'form-control','style'=>'display:none;']) !!}
+    {!! $errors->first('description','<p class="help-block">:message</p>')!!}
+  </div>
+  <div class="summernote"></div>
 </div>
 <div class="col-md-12">
   <div class="form-group {!! $errors->has('categories') ? 'has-error':'' !!}">
